35154a81ba549f24c0d3ff611afa9b007dd23624
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-12-14 12:17:20 -0300

    Add files via upload

---
+++ Dominando os Operadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string titulo = "";
+        string descricao = "";
+        string dataVencimento = "";
+
+        titulo = Console.ReadLine();
+        descricao = Console.ReadLine();
+
+        if (descricao.Length > 50)
+        {
+            Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+        }
+        else
+        {
+            dataVencimento = Console.ReadLine();
+
+            Console.WriteLine($"{descricao} ate {dataVencimento}");
+        }
+    }
+}
