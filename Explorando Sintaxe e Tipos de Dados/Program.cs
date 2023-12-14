0225b84a755ae1133aad28852aeec7c9fa7071fb
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-12-14 11:20:32 -0300

    Add files via upload
    Primeiro desafio de código do Bootcamp DecolaTech 2024 AVANADE.

---
+++ Explorando Sintaxe e Tipos de Dados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    string registroEmail = "";
+    string registroNome = ""; 
+    string senha = "";
+
+    registroEmail = Console.ReadLine();
+    registroNome = Console.ReadLine();
+    senha = Console.ReadLine();
+
+    Console.WriteLine($"{registroNome}, verifique o email: {registroEmail} para ativar.");
+
+    }
+}
