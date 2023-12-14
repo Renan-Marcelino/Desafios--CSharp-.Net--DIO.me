ab213ddcd1888dcface4d4f350f24d0907a5f868
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-12-14 13:09:00 -0300

    Add files via upload

---
+++ Criando um Objeto com Atributos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Jogador
+{
+    public string Nome { get; set; }
+    public string Nacionalidade { get; set; }
+    public int Idade { get; set; }
+    public string Posicao { get; set; }
+
+    public Jogador()
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string Nome = Console.ReadLine();
+        string Nacionalidade = Console.ReadLine();
+        int Idade = Convert.ToInt32(Console.ReadLine());
+        string Posicao = Console.ReadLine();
+        
+        Console.WriteLine($"Jogador criado!\n{Nome}\n{Nacionalidade}\n{Idade}\nPosição: {Posicao}");
+      
+    }
+}
---
+++ Desvendando o Controle de Fluxo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    int limiteInferior = int.Parse(Console.ReadLine());
+    int limiteSuperior = int.Parse(Console.ReadLine());
+
+    int somaPares = 0;
+
+    for (int i = limiteInferior; limiteSuperior >= i; i ++){
+      if (i % 2 == 0){
+        somaPares = somaPares + i;
+      }
+    }  
+    
+    Console.WriteLine($"A soma dos numeros pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
+    }
+}
