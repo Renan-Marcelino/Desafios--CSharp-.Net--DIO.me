f6259f3eed2a08d8f9e92e7d898c48538b5191d0
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-12-14 13:06:26 -0300

    Add files via upload

---
+++ Utilizando Propriedades e Métodos/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Robo
+{
+    public int VelocidadeAtual { get; set; } = 0;
+    public int VelocidadeMaxima { get; }
+    public int VelocidadeMinima { get; }
+
+    public Robo(int vmin, int vmax)
+    {
+        VelocidadeMinima = vmin;
+        VelocidadeMaxima = vmax;
+        VelocidadeAtual = vmin;
+    }
+
+    public void Acelerar()
+    {
+        if (VelocidadeAtual < VelocidadeMaxima)
+        {
+            VelocidadeAtual++;
+        }
+    }
+
+    public void Desacelerar()
+    {
+        if (VelocidadeAtual > VelocidadeMinima)
+        {
+            VelocidadeAtual--;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputV = Console.ReadLine().Split(' ');
+        int vmin = int.Parse(inputV[0]);
+        int vmax = int.Parse(inputV[1]);
+
+        string comandos = Console.ReadLine();
+
+        Robo robo = new Robo(vmin, vmax);
+
+        foreach (char comando in comandos)
+        {
+            if (comando == 'A')
+            {
+                robo.Acelerar();
+            }
+            else if (comando == 'D')
+            {
+                robo.Desacelerar();
+            }
+        }
+        Console.WriteLine(robo.VelocidadeAtual);
+    }
+}
